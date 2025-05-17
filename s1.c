ec19ed7f2224e389f02049d303c6c9df97ad7fd2
arjunb2206 <arjun.b@s.amity.edu>
2025-05-17 11:49:32 +0530

    calc made in c|s1.c

---
+++ s1.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,i;
+printf("enter the number 1,add\n 2,sub \n 3,multiply\n 4,div");
+scanf("%d",&i);
+switch(i)
+{
+	case 1 :
+		printf("enter two number");
+		scanf("%d%d",&a,&b);
+		c=a+b;
+		printf("sum is:%d ",c);
+		break ;
+        case 2 :
+		 printf("enter two number");
+                scanf("%d%d",&a,&b);
+		c=a-b;
+		printf("sub is:%d",c);
+		break ;
+	case 3 :
+		 printf("enter two number");
+                scanf("%d%d",&a,&b);
+		c=a*b;
+	        printf("multiplty is:%d",c);
+                break;
+	case 4:
+		 printf("enter two number");
+                scanf("%d%d",&a,&b);
+		c=a/b;
+		printf("div is:%d",c);
+		break;
+       default :
+		printf("invalid input:");
+}
+return 0;
+}
