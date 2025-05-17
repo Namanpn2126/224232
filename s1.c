dca63b39b0a838c270fec5e8ae72ef31b8ace2ee
Namanpn2126 <naman.n@s.amity.edu>
2025-05-17 16:29:25 +0530

    changed in c|s1.c

--- s1.c
+++ s1.c
@@ -1,27 +1,27 @@
 #include<stdio.h>
 int main()
 {
-int a,b,c,d,i;
+int x,y,z,n,i;
 printf("enter the number 1,add\n 2,sub \n 3,multiply\n 4,div");
 scanf("%d",&i);
 switch(i)
 {
 	case 1 :
-		c=a+b+d;
-	        printf("sum is:%d ",c);
+		n=x+y+z;
+	        printf("sum is:%d ",n);
 		break ;
         case 2 :
-		c=a-b-d;
-		printf("sub is:%d",c);
+		n=x-y-z;
+		printf("sub is:%d",n);
 		break ;
 	case 3 :
-		c=a*b*d;
-	        printf("multiplty is:%d",c);
+		n=x*y*z;
+	        printf("multiplty is:%d",n);
                 break;
 	case 4:
-		c=a/b;
-		c=c/d;
-		printf("div is:%d",c);
+		n=x/y;
+		n=y/z;
+		printf("div is:%d",n);
 		break;
        default :
 		printf("invalid input:");
